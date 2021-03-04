5ced5ee82b14b50288dd2092e3301d0f64a2ad83
kodycn <55975390+kodycn@users.noreply.github.com>
2021-03-04 15:55:21 -0800

    added the necessary changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
+const dbURI = 'mongodb+srv://kodycn:kodycn123@cluster0.ezgjo.mongodb.net/node-auth?';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
